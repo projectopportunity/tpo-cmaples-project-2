5e6efaed8b59f2dbf583b94a6dc4d8b42a4fc4b9
tpo-cmaples <77011904+tpo-cmaples@users.noreply.github.com>
2021-01-21 11:56:33 -0500

    updated with html vs creating elements

--- js/script.js
+++ js/script.js
@@ -17,54 +17,31 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-function showPage (list, page){
-   const startIndex = (page * 9)-9;
-   const endIndex = page * 9;
+function showPage (list, pageNumber){
+   let endIndex = pageNumber * 9;
+   const startIndex = endIndex-9;
+   if (endIndex > list.length){
+      endIndex = list.length;
+   };
+
    const studentList = document.querySelector('.student-list');
    studentList.innerHTML = '';  
 
    for (i = startIndex; i < endIndex; i++){
-      
-      function newElement (element, type, value){
-         const newElement = document.createElement(element);
-         newElement[type] = value;
-         return newElement;
-         
-      };
-      
-      //create list item
-      const li = newElement('li', 'className', 'student-item cf');
-      //create a div for student deatils
-      const divStudentDetails = newElement('div', 'className', 'student-details');
-      //create img tag
-      const img = newElement('img', 'className', 'avatar');
-      img.src = list[i].picture.thumbnail;
-      img.alt = 'Profile Picture';
-      //create name heading
-      const fullName = `${list[i].name.title} ${list[i].name.first} ${list[i].name.last}`;
-      const student = newElement('h3', 'textContent', fullName);
-      //create span class for email
-      const spanEmail = document.createElement('span');
-      spanEmail.className = 'email';
-      spanEmail.textContent = list[i].email;
-      //div for joined details
-      const divJoinedDetails = newElement('div', 'className', 'joined-details');
-      //span class for date joined
-      const spanDateJoined = newElement('span', 'className', 'date');
-      spanDateJoined.textContent = list[i].registered.date;
-
-      li.appendChild(divStudentDetails);
-      li.appendChild(img);
-      li.appendChild(student);
-      li.appendChild(spanEmail);
-      li.appendChild(divJoinedDetails);
-      li.appendChild(spanDateJoined);
-
-      studentList.appendChild(li);
-
+      studentList.insertAdjacentHTML('beforeend',`
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${list[i].picture.thumbnail}" alt="Profile Pictures">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>
+      `);
    };
 };
-
 
 /*
 Create the `addPagination` function
@@ -74,23 +51,12 @@ function addPagination(list, activePage){
    const pageNumberList = document.querySelector('.link-list');
    const buttonCount = Math.ceil(list.length / 9);
    pageNumberList.innerHTML = '';
-
-   for (i = 1; i <= buttonCount; i++){
-      const pageList = document.createElement('li');
-      const pageButton = document.createElement('button');
-      pageButton.textContent = i;
-      pageButton.type = 'button';
-
-      //if active page number equals i set classname to active
-      //pageButtons.className = 'active';
-      
-      if(pageButton.textContent ===activePage){
-         pageButton.className = 'active';
-      };
-
-      pageList.appendChild(pageButton);
-      pageNumberList.appendChild(pageList);
-      
+   for (i=1; i<= buttonCount; i++){
+      pageNumberList.insertAdjacentHTML('beforeend',`
+         <li>
+            <button type="button" class="">${i}</button>
+         </li>
+      `);
    };
 };
 
@@ -98,14 +64,14 @@ function addPagination(list, activePage){
 showPage(data, 1);
 addPagination(data, '1');
 const pageUl = document.querySelector('.link-list'); 
+pageUl.firstElementChild.firstElementChild.className="active";
 
 pageUl.addEventListener('click', (e)=>{
    
    if (e.target.tagName ==='BUTTON'){  
       const activePage = e.target.textContent;
-      addPagination(data, activePage); 
-      showPage(data, activePage);
-      
+      document.querySelector('.active').className = '';
+      e.target.className = 'active';
+      showPage(data, activePage);   
    };
-
 });
